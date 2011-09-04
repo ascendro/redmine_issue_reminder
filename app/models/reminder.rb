ffa7e6a07f2114fb2cf0ad7cd8a716a0e84b136e
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2011-09-04 14:59:12 +0200

    Displaying proper interval values

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -35,13 +35,28 @@ class Reminder < ActiveRecord::Base
   def self.interval_values_for(interval)
     case interval
     when("daily")
-      daily_intervals.each_with_index.collect {|val,idx| [l(val), idx]}
+      daily_intervals.each_with_index.collect {|val,idx| [interval_value_display("daily", val), idx]}
     when("weekly")
-      weekly_intervals.each_with_index.collect {|val,idx| [l(:every_weekly_format) % l(val.downcase.to_sym), idx]}
+      weekly_intervals.each_with_index.collect {|val,idx| [interval_value_display("weekly", val), idx]}
     when("monthly")
-      monthly_intervals.each_with_index.collect {|val,idx| [l(:every_of_month_format) % val, idx]}
+      monthly_intervals.each.collect {|val| [interval_value_display("monthly", val), val]}
     else
       []
+    end
+  end
+
+  def self.interval_value_display(interval, value)
+    case interval
+    when("daily")
+      value = Reminder.daily_intervals[value] if value.is_a? Integer
+      l(value)
+    when("weekly")
+      value = Date::DAYNAMES[value] if value.is_a? Integer
+      l(:every_weekly_format) % l(value.downcase.to_sym)
+    when("monthly")
+      l(:every_of_month_format) % value
+    else
+      "Unknown"
     end
   end
 
