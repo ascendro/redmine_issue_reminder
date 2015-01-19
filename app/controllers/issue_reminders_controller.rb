f9bf0044318f1f2496fd999b25b6aa279a30c35e
Sergio Díaz <sergio.diaz@mo2o.com>
2015-01-19 17:13:41 +0100

    Some modifications in the controller for the new name

--- app/controllers/issue_reminders_controller.rb
+++ app/controllers/issue_reminders_controller.rb
@@ -19,13 +19,13 @@ class IssueRemindersController < ApplicationController
   end
 
   def create
-    reminder = IssueReminder.new(params[:reminder])
+    reminder = IssueReminder.new(params[:issue_reminder])
     reminder.interval_value = params[:interval_value].to_i
     if reminder.save
       Role.find_all_givable.each do |role|
         if params[role.name.downcase]
           rr = ReminderRole.new
-          rr.reminder = reminder
+          rr.issue_reminder = reminder
           rr.role = role
           rr.save
         end
@@ -40,14 +40,14 @@ class IssueRemindersController < ApplicationController
 
   def update
     reminder = IssueReminder.find(params[:id])
-    if request.put? && reminder.update_attributes(params[:reminder])
+    if request.put? && reminder.update_attributes(params[:issue_reminder])
       reminder.interval_value = params[:interval_value]
       Role.find_all_givable.each do |role|
         if reminder.roles.include?(role) && params[role.name.downcase].nil?
           reminder.reminder_roles.find_by_role_id(role.id).destroy
         elsif params[role.name.downcase] && !reminder.roles.include?(role)
           rr = ReminderRole.new
-          rr.reminder = reminder
+          rr.issue_reminder = reminder
           rr.role = role
           rr.save
         end
@@ -69,7 +69,7 @@ class IssueRemindersController < ApplicationController
   def update_interval_values
     vals = IssueReminder.interval_values_for(params[:interval])
     begin
-      reminder = IssueReminder.find(params[:reminder_id])
+      reminder = IssueReminder.find(params[:issue_reminder_id])
     rescue ActiveRecord::RecordNotFound
       reminder = IssueReminder.new
     end
@@ -87,7 +87,7 @@ class IssueRemindersController < ApplicationController
     begin
       @project = Project.find(params[:project_id])
     rescue ActiveRecord::RecordNotFound
-      @project = Project.find(params[:reminder][:project_id]) if params[:reminder]
+      @project = Project.find(params[:issue_reminder][:project_id]) if params[:issue_reminder]
     end
   end
 end
